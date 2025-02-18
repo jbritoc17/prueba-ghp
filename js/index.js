b2b2331f7208d0a9d1811a8c8d5f9b376f3bb82c
JeanDev <jbritoc17@outlook.es>
2025-02-18 11:05:57 -0500

    changes in process

--- js/index.js
+++ js/index.js
@@ -1,43 +1,35 @@
 
 
     // JavaScript para cambiar la clase del navbar al hacer scroll
+
     $(window).scroll(function () {
         if ($(this).scrollTop() > 1) {
             $('.bg-body-tertiary').addClass('transparent');
+            $('.text-dalany2').addClass('text-dalany2-scrollHover');
+            
         } else {
             $('.bg-body-tertiary').removeClass('transparent');
+            $('.text-dalany2').removeClass('text-dalany2-scrollHover');
         }
     });
 
-//cambiar color de letras de los elementos del navbar al hacer scroll
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 1) {
-            $('.nav-link').addClass('text-white');
-        } else {
-            $('.nav-link').removeClass('text-white');
-        }
-    });
-$(window).scroll(function () {
-    if ($(this).scrollTop() >1){
-        $('.navbar-brand').addClass('text-white');
-    }else {
-        $('.navbar-brand').removeClass('text-white');
-    }
-});	
-    
+
+    /* 
     $(window).scroll(function () {
         let scroll = $(window).scrollTop();
         $('.parallax-bg').css('background-position', '50%'+ (-scroll / 3) + 'px');
-    });
+    }); */
   
 
-    /* cambiar icono */
+    /* cambiar  bg navbar*/
     $(window).scroll(function () {
         if($(this).scrollTop() >1){
             $('.navbar').addClass('transparent');
-            $('.navbar-brand img').attr('src','./assets/imgs/icono2.ico');
+           
         }else {
             $('.navbar').removeClass('transparent');
-            $('#icono').attr('src', './assets/imgs/DALANY-logo.ico');
+           
         }
     }   );
+
+    
